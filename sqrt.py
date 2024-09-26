ddc446312a3dabd439f2e590416766570feda96d
AR-Pial <88152518+AR-Pial@users.noreply.github.com>
2024-09-26 13:19:57 +0600

    sqrt

--- sqrt.py
+++ sqrt.py
@@ -1,15 +1,26 @@
+# def mySqrt(x):
+#     if x == 0 or x == 1:
+#         return x
+#     else:
+#         i = 2
+#         while i <= x:
+#             square = i * i
+#             if square > x:
+#                 return i-1
+#             i += 1
 def mySqrt(x):
-    if x == 0 or x == 1:
-        return x
-    else:
-        i = 2
-        while i <= x:
-            square = i * i
-            if square > x:
-                return i-1
-            i += 1
-
+    left = 0
+    right = x
+    while left <= right:
+        mid = (left + right) // 2
+        mid_sq = mid * mid
+        if mid_sq == x:
+            return mid
+        elif mid_sq < x:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return right
 
 result = mySqrt(8)
-
 print(result)
