802ddbb79ed339213da74119b88612f315f084f8
AR-Pial <arpialcodes@gmail.com>
2025-04-23 01:28:57 +0600

    remive element

---
+++ easy_LeetCode/remove_element.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
+
+nums = [3,2,2,3,4]
+val = 3
+sol = Solution()
+res = sol.removeElement(nums,val)
+print(res)
